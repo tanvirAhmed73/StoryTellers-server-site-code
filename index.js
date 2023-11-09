cfb89dba833606acf4e9687cd5a53f0ae7681bec
Tanvir Ahmed <tanvir734215@gmail.com>
2023-11-09 17:25:21 +0600

    return button added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");//npm i jsonwebtoken
 const app = express();
+const cookieParser = require('cookie-parser')
 const port = process.env.port || 5000;
 require('dotenv').config()
 
 // middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:['http://localhost:5173'],
+    credentials:true
+  }
+));
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 
@@ -29,9 +36,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // jwt middleware
+    const verify = async(req,res,next) =>{
+      // cookies ache kina check koro
+      const token = req.cookies?.token;
+      if(!token){
+        res.status(401).sent({status : "unAuthorized Access", code:"401"})
+      }
+      jwt.verify(token, process.env.Secret_key,(error,decode)=>{
+        if(error){
+          res.status(401).sent({status : "unAuthorized Access", code:"401"})
+        }else{
+          // console.log(decode);
+          req.decode = decode;
+        }
+      });
+      next();
+    }
+
+    // database
     const database = client.db("bookDB");
     const productCollection=database.collection("book");
-
+    const borrowCollection=database.collection("borrowBook");
+    
+    // Add error handling
+    try {
+      // Attempt to create the "catagorie" collection
+      await borrowCollection.insertOne({}); // This will not actually insert data, but it will create the collection
+       // This will not actually insert data, but it will create the collection
+      console.log("Collection 'catagorie' created successfully.");
+    } catch (error) {
+      console.error("Error creating 'catagorie' collection:", error);
+    }
     // add product of get
     app.get('/book', async(req,res)=>{
       const cursor = productCollection.find();
@@ -45,6 +81,27 @@ async function run() {
       const result = await productCollection.findOne(query);
       res.send(result);
     })
+
+    app.get('/addborrow',  async(req,res)=>{
+      const cursor = borrowCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    // add to borrow section
+    app.post('/addborrow',  async(req,res)=>{
+      try{
+        const addborrow = req.body;
+        const result = await borrowCollection.insertOne(addborrow);
+        res.send(result);
+      }
+      catch(error){
+        console.log(error);
+        res.send(error); 
+      }
+      
+  })
 
 
     // updated product
@@ -68,16 +125,44 @@ async function run() {
       res.send(result)
     })
 
+    app.post("/jwt", async(req,res)=>{
+      const body = req.body;
+      // jwt.sign("payload","secretKey","expireInfo");
+      const token = jwt.sign(body,process.env.Secret_key, {expiresIn : "10h" });
+      const expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 7)
+      res.cookie ("token",token,{
+        httpOnly:true,
+        secure: false,
+        expires:expirationDate,
+      }).send({msg:"Succeed"})
+      // res.send({body,token});
+    })
+
 
     // add product
-    app.post('/book', async(req,res)=>{
-      const newBook = req.body;
-      const result = await productCollection.insertOne(newBook);
-      res.send(result);
+        // res.status(401).sent({status : "unAuthorized Access", code:"401"})
+      app.post('/book',  async(req,res)=>{
+      try{
+        const newBook = req.body;
+        const result = await productCollection.insertOne(newBook);
+        res.send(result);
+      }catch(error){
+        console.log(error);
+        res.send(error); 
+      }
+
+      app.delete('/addborrow/:id', async(req,res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+      })
+
       
   })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
